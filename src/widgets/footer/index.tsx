2e473bb3b87cb7d03cfb76d8041f11435aa53895
Zlis7 <denkalmy@gmail.com>
2025-05-31 17:32:56 +0500

    Prettier

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -1,10 +1,12 @@
 export const Footer: React.FC = () => {
   return (
     <>
-      <footer className="flex items-top justify-between w-full px-20 pt-20">
-        
+      <footer className="items-top flex w-full justify-between px-20 pt-20">
         <div className="w-65">
-          <img src="./tempLogo.png" alt="logo" />
+          <img
+            src="./tempLogo.png"
+            alt="logo"
+          />
 
           <p className="text-xs text-gray-500">
             ООО "Сил-Сервис" Все права защищены
@@ -19,13 +21,15 @@ export const Footer: React.FC = () => {
           <a>Задать вопрос</a>
         </div>
 
-        <div className='w-1/3'>
+        <div className="w-1/3">
           <p>Контакты</p>
 
           <div>
             <p>Городской офис</p>
             <p>454008, г. Челябинск, Свердловский тракт, 24 </p>
-            <p>Телефон/факс: <a href="tel:+73517990151">+7(351)799-01-51</a></p>
+            <p>
+              Телефон/факс: <a href="tel:+73517990151">+7(351)799-01-51</a>
+            </p>
           </div>
 
           <div>
@@ -34,7 +38,9 @@ export const Footer: React.FC = () => {
               456518, Россия, Челябинская область, Сосновский р-н, п.
               Долгодеревенское, д. Ключевка ОК "Лесная застава"
             </p>
-            <p>Телефон/факс: <a href="tel:+73512694916">+7(351)269-49-16</a></p>
+            <p>
+              Телефон/факс: <a href="tel:+73512694916">+7(351)269-49-16</a>
+            </p>
             <p>Email: ok-zastawa@yandex.ru</p>
             <p>Skype: okzastava</p>
           </div>
