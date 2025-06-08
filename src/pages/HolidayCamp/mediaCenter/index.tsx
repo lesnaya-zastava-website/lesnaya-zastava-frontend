b22e608d8277b49ae4969f6f486f3d20b45a5be7
Denis Kalmykov <141158286+Zlis7@users.noreply.github.com>
2025-06-08 23:31:16 +0500

    Feature/changing components (#7)
    * Хотфикс
    
    * удаление лишних компонентов

--- src/pages/HolidayCamp/mediaCenter/index.tsx
+++ src/pages/HolidayCamp/mediaCenter/index.tsx
@@ -1,38 +1,37 @@
 import { CardMediaCenter } from '@/shared/ui/CardMediaCenter';
-import { CardSection } from '@/shared/ui/CardSection';
-
 
 export const MediaCenter: React.FC = () => {
-  const media = {
-    dataMedia: [
-      {
-        srcImg: '/vite.svg',
-        date: '15 мая 2025',
-        title: 'Открытие летнего сезона 2025!',
-        message: 'Приглашаем всех на открытие летнего сезона в нашем лагере!'
-      },
-      {
-        srcImg: '/vite.svg',
-        date: '10 мая 2025',
-        title: 'Новая программа "Юный исследователь"',
-        message: 'Представляем новую образовательную программу этого лета!'
-      },
-    ]
-  };
-  
+  const dataMedia = [
+    {
+      srcImg: '/vite.svg',
+      altImg: undefined,
+      date: '15 мая 2025',
+      title: 'Открытие летнего сезона 2025!',
+      message: 'Приглашаем всех на открытие летнего сезона в нашем лагере!',
+    },
+    {
+      srcImg: '/vite.svg',
+      altImg: undefined,
+      date: '10 мая 2025',
+      title: 'Новая программа "Юный исследователь"',
+      message: 'Представляем новую образовательную программу этого лета!',
+    },
+  ];
+
   return (
     <section className="mx-auto px-4 sm:px-6 md:px-10 lg:px-20">
-      <div className="container mx-auto border-b border-gray-200 pb-8">
-          <CardSection
-          icon="[📷]"
-          title="Медиа-центр лагеря"
-          /*@ts-ignore*/
-          Component={CardMediaCenter}
-          data={media}
-          isPositionLeft={true}
-          isContainer={false}
-          isLoop={false}
-        />
+      <div className="container mx-auto pb-8">
+        <section className="flex flex-col items-center gap-8 py-16 md:flex-row">
+          <div className="md:w-1/3">
+            <div className="mb-4 flex items-center gap-2">
+              <span className="font-mono text-gray-700">[📷]</span>
+              <h2 className="text-2xl font-semibold">Медиа-центр лагеря</h2>
+            </div>
+          </div>
+          <div className="md:w-2/3">
+            <CardMediaCenter dataMedia={dataMedia} />
+          </div>
+        </section>
       </div>
     </section>
   );
