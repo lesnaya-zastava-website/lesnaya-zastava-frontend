59f739dd2b571519be90acae910fbfae277ab023
Denis Kalmykov <141158286+Zlis7@users.noreply.github.com>
2025-06-21 17:18:07 +0000

    тестирование документации (#10)

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,71 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'ОК "Лесная застава"',
+  description: 'Оздоровительный комплекс "Лесная застава"',
+  themeConfig: {
+    logo: '/images/tempLogo.png',
+    siteTitle: 'ОК "Лесная застава"',
+    nav: [
+      { text: 'Главная', link: '/' },
+      {
+        text: 'Руководство',
+        items: [
+          { text: 'Администратора', link: '/admin/auth' },
+          { text: 'Программиста', link: '/expert/dependencies' },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        text: 'Введение',
+        items: [
+          { text: 'Начало работы', link: '/intro/start' },
+          { text: 'Рекомендации', link: '/intro/recommendations' },
+          { text: 'Проблемы', link: '/intro/problems' },
+        ],
+      },
+      {
+        text: 'Для администратора',
+        items: [
+          { text: 'Авторизация', link: '/admin/auth' },
+          { text: 'Интерфейс панели', link: '/admin/ui' },
+          { text: 'Медиафайлы', link: '/admin/media' },
+          { text: 'Управление контентом', link: '/admin/management' },
+        ],
+        collapsed: true,
+      },
+      {
+        text: 'Для программиста',
+        items: [
+          { text: 'Зависимости', link: '/expert/dependencies' },
+          { text: 'Архитектура', link: '/expert/architecture' },
+          { text: 'Маршрутизация', link: '/expert/routing' },
+          { text: 'Страницы', link: '/expert/pages' },
+          { text: 'Компоненты', link: '/expert/components' },
+          { text: 'Админ панель', link: '/expert/server' },
+          { text: 'Документация', link: '/expert/docs' },
+        ],
+        collapsed: true,
+      },
+    ],
+    footer: {
+      copyright: 'ООО "Соц-сервис" Все права защищены',
+    },
+    docFooter: {
+      prev: 'Прошлая страница',
+      next: 'Следующая страница',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/lesnaya-zastava-website' },
+    ],
+  },
+  locales: {
+    root: {
+      label: 'Русский',
+      lang: 'ru',
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+layout: home
+
+hero:
+  name: Лесная застава
+  tagline: Документация применимых решений
+  actions:
+    - theme: brand
+      text: Начало работы
+      link: /intro/start
+    - theme: alt
+      text: Руководство администратора
+      link: /admin/auth
+    - theme: alt
+      text: Руководство программиста
+      link: /expert/dependencies
+    - theme: alt
+      text: Github
+      link: https://github.com/lesnaya-zastava-website
+  image:
+    src: /images/tempLogo.png
+    alt: VitePress
+features:
+  - icon: 🛠️
+    title: Панель администратора
+    details: Описание управления контентом сайта и медиаконтентом.
+  - icon: 📝
+    title: Структура проекта
+    details: Описание архитектуры проекта и использованных решений.
+  - icon: 💡
+    title: Рекомендации
+    details: Описание правил по лучшему использованию админ панели и алгоритм поведения при обнаружении ошибок.
+---
