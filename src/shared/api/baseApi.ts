c90cfc68708622384adc508d977584a646fb0694
cm3tahkuh <markplay13498@gmail.com>
2025-06-27 05:46:40 +0500

    вынос api ссылки в env

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const API_BASE_URL = "http://localhost:1337";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
-  baseURL: API_BASE_URL + "/api",
+  baseURL: API_BASE_URL + '/api',
 });
